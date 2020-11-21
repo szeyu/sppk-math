c2c8d2b073af41473447b7f39f189de8c9e6e270
szeyu <szeyusim@gmail.com>
2020-11-21 18:24:00 +0800

    change the code to shorter one and ask SO question

--- createGuru.php
+++ createGuru.php
@@ -66,8 +66,8 @@
                             <option value="Measure of Dispersion"></option>
                             <option value="Combined Event"></option>
                             <option value="Dependent Events and Independent Events"></option>
-                            <option value="Mutually EXclusive Events and Non-Mutually Exclusive Events"></option>
-                            <option value="Aplication of Probability of Combined Events"></option>
+                            <option value="Mutually Exclusive Events and Non-Mutually Exclusive Events"></option>
+                            <option value="Application of Probability of Combined Events"></option>
                             <option value="Financial Planning of combined Events"></option>
                         </datalist>
                        
@@ -124,7 +124,7 @@
                     <br>
                     
                     <!-- add new soalan here -->
-                    <p id="soalanDitambah"></p>
+                    <div id="soalanDitambah"></div>
 
                 </form>
                 
@@ -139,223 +139,29 @@
                     function tambahSoalan(){
                         noSoalanCounter++;
 
-                        // get form element id
-                        var formSoalan = document.getElementById("soalanForm");
-                        var formPilihan = document.getElementById("soalanForm");
-                        var formJawapan = document.getElementById("soalanForm");
-
-                        // create label element
-                        //label for "Soalan 2,3,4,5..."
-                        var labelSoalan = document.createElement("label");         
-                        labelSoalan.setAttribute("for","soalan");
-                        var textSoalan = document.createTextNode("Soalan " + noSoalanCounter +":");
-                        labelSoalan.appendChild(textSoalan);
-                        
-
-                        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        // label for "Jawapan :"
-                        var labelJawapan = document.createElement("label");
-                        labelJawapan.setAttribute("for","jawapan" + noSoalanCounter);
-                        labelJawapan.setAttribute("name","jawapan" + noSoalanCounter);        
-                        labelJawapan.setAttribute("id","jawapan" + noSoalanCounter);
-                        //labelJawapan.setAttribute("class","jawapanBox");
-                        labelJawapan.style.textAlign = "left";
-                        labelJawapan.style.margin = "auto";
-                        var textJawapan = document.createTextNode("Jawapan : ");
-                        labelJawapan.appendChild(textJawapan);
-
-                        // label for "Pilihan A"
-                        var labelPilihanA = document.createElement("label");        
-                        labelPilihanA.setAttribute("for","pilihanA");
-                        //labelPilihanA.setAttribute("class","pilihanBox");
-                        labelPilihanA.style.textAlign = "left";
-                        var textPilihanA = document.createTextNode("pilihan A :");
-                        labelPilihanA.appendChild(textPilihanA);
-                        
-                        //label for "Pilihan B"
-                        var labelPilihanB = document.createElement("label");           
-                        labelPilihanB.setAttribute("for","pilihanB");
-                        //labelPilihanB.setAttribute("class","pilihanBox");
-                        labelPilihanB.style.textAlign = "left";
-                        var textPilihanB = document.createTextNode("pilihan B :");
-                        labelPilihanB.appendChild(textPilihanB);
-
-                        //label for "Pilihan C"
-                        var labelPilihanC = document.createElement("label");        
-                        labelPilihanC.setAttribute("for","pilihanC");
-                        //labelPilihanC.setAttribute("class","pilihanBox");
-                        labelPilihanC.style.textAlign = "left";
-                        var textPilihanC = document.createTextNode("pilihan C :");
-                        labelPilihanC.appendChild(textPilihanC);
-
-                        //label fot "Pilihan D"
-                        var labelPilihanD = document.createElement("label");        
-                        labelPilihanD.setAttribute("for","pilhanD");
-                        //labelPilihanD.setAttribute("class","pilihanBox");
-                        labelPilihanD.style.textAlign = "left";
-                        var textPilihanD = document.createTextNode("pilihan D :");
-                        labelPilihanD.appendChild(textPilihanD);
-
-
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        // Create input element
-                        // soalan input
-                        var soalan = document.createElement("input");
-                        soalan.setAttribute("type","text");
-                        soalan.setAttribute("placeholder","Soalan " + noSoalanCounter);
-                        soalan.setAttribute("id",noSoalan);
-                        soalan.setAttribute("name",noSoalan);
-
-
-                        /*border-radius: 5px;
-                        border: 2px solid rgba(83, 82, 82, 0.527);
-                        font-size: large;
-                        padding-left: 10px;
-                        padding-right: 10px;
-                        padding-bottom: 5px;
-                        margin-top: 2px;*/ 
-
-                        // jawapan dropdown menu input
-                        var jawapan = document.createElement("select");
-                        //jawapan.setAttribute("class","jawapanBox");
-                        jawapan.style.float = "left";
-                        //jawapan.style.padding = "5px";
-                        jawapan.style.margin = "auto";
-                        jawapan.style.width = "4.5%";
-                        jawapan.style.fontSize = "large";
-                        jawapan.style.borderRadius = "5px";
-                        jawapan.style.marginTop = "2px";
-                        jawapan.style.marginLeft = "3.5%";
-                        jawapan.style.paddingLeft = "10px";
-                        jawapan.style.paddingRight = "10px";
-                        jawapan.style.paddingBottom = "5px";
-                        jawapan.style.border = "2px solid rgba(83,82,82,0.5)";
-                        jawapan.style.display = "inline-block";
-                        jawapan.setAttribute("name","jawapan"+noSoalanCounter);
-                        jawapan.setAttribute("id","jawapan"+noSoalanCounter);
-                        
-                        var jawapanA = document.createElement("option"); 
-                        jawapanA.setAttribute("value", "A"); 
-						var nodJawapanA = document.createTextNode("A"); 
-						jawapanA.appendChild(nodJawapanA); 
-						jawapan.appendChild(jawapanA); 
-                        
-                        var jawapanB = document.createElement("option"); 
-                        jawapanB.setAttribute("value", "B"); 
-						var nodJawapanB = document.createTextNode("B"); 
-						jawapanB.appendChild(nodJawapanB); 
-						jawapan.appendChild(jawapanB); 
-                        
-                        var jawapanC = document.createElement("option"); 
-                        jawapanC.setAttribute("value", "C"); 
-						var nodJawapanC = document.createTextNode("C"); 
-						jawapanC.appendChild(nodJawapanC); 
-						jawapan.appendChild(jawapanC); 
-                        
-                        var jawapanD = document.createElement("option"); 
-                        jawapanA.setAttribute("value", "D"); 
-						var nodJawapanD = document.createTextNode("D"); 
-						jawapanD.appendChild(nodJawapanD); 
-						jawapan.appendChild(jawapanD); 
-
-
-                        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        //input for pilihan A
-                        var pilihanA = document.createElement("input");
-                        //pilihanA.setAttribute("class","pilihanBox");
-                        pilihanA.style.width = "26%";
-                        pilihanA.style.marginRight = "14%";
-                        pilihanA.setAttribute("type","text");
-                        pilihanA.setAttribute("placeholder","Pilihan A Soalan " + noSoalanCounter);
-                        pilihanA.setAttribute("id","pilihanA" + noSoalanCounter);
-                        pilihanA.setAttribute("name","pilihanA" + noSoalanCounter);                       
-                        
-                        //input for pilihan B
-                        var pilihanB = document.createElement("input");
-                        //pilihanB.setAttribute("class","pilihanBox");
-                        pilihanB.style.width = "26%";
-                        pilihanB.style.marginRight = "14%";
-                        pilihanB.setAttribute("type","text");
-                        pilihanB.setAttribute("placeholder","Pilihan B Soalan " + noSoalanCounter);
-                        pilihanB.setAttribute("id","pilihanA" + noSoalanCounter);
-                        pilihanB.setAttribute("name","pilihanA" + noSoalanCounter);
-                                               
-                        //input for pilihan C
-                        var pilihanC = document.createElement("input");
-                        //pilihanC.setAttribute("class","pilihanBox");
-                        pilihanC.style.width = "26%";
-                        pilihanC.style.marginRight = "14%";
-                        pilihanC.setAttribute("type","text");
-                        pilihanC.setAttribute("placeholder","Pilihan C Soalan " + noSoalanCounter);
-                        pilihanC.setAttribute("id","pilihanA" + noSoalanCounter);
-                        pilihanC.setAttribute("name","pilihanA" + noSoalanCounter);                        
-                        
-                        //input for pilihan D
-                        var pilihanD = document.createElement("input");
-                        // pilihanD.setAttribute("class","pilihanBox");
-                        pilihanD.style.width = "26%";
-                        pilihanD.style.marginRight = "14%";
-                        pilihanD.setAttribute("type","text");
-                        pilihanD.setAttribute("placeholder","Pilihan D Soalan " + noSoalanCounter);
-                        pilihanD.setAttribute("id","pilihanA" + noSoalanCounter);
-                        pilihanD.setAttribute("name","pilihanA" + noSoalanCounter);
-                        
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        // put Soalan 2,3,4...  : __________
-                        formSoalan.appendChild(labelSoalan);
-                        formSoalan.appendChild(soalan);
-                        formSoalan.appendChild(br.cloneNode());  
-                        formSoalan.appendChild(br.cloneNode());  
-
-                        //////////////////////////////////////////
-                        formJawapan.appendChild(labelJawapan);
-                        formJawapan.appendChild(jawapan);
-                        formJawapan.appendChild(br.cloneNode()); 
-                        formJawapan.appendChild(br.cloneNode()); 
-                        formJawapan.appendChild(br.cloneNode()); 
-                        formJawapan.appendChild(br.cloneNode()); 
-                        formJawapan.appendChild(br.cloneNode()); 
-
-
-                        ////////////////////////////////////////
-
-                        formPilihan.appendChild(labelPilihanA);
-                        formPilihan.appendChild(pilihanA);
-                        formPilihan.appendChild(br.cloneNode());  
-                        //formPilihan.appendChild(br.cloneNode());
-
-                        formPilihan.appendChild(labelPilihanB);
-                        formPilihan.appendChild(pilihanB);
-                        formPilihan.appendChild(br.cloneNode());  
-                        //formPilihan.appendChild(br.cloneNode());
-
-                        formPilihan.appendChild(labelPilihanC);
-                        formPilihan.appendChild(pilihanC);
-                        formPilihan.appendChild(br.cloneNode());  
-                        //formPilihan.appendChild(br.cloneNode());
-
-                        formPilihan.appendChild(labelPilihanD);
-                        formPilihan.appendChild(pilihanD);
-                        formPilihan.appendChild(br.cloneNode());  
-                        formPilihan.appendChild(br.cloneNode());
+                        var divForm = document.createElement('div');
+                        divForm.innerHTML += "<div id= 'soalanForm" + noSoalanCounter +"'><br><br><br><hr><br><label for=soalan> Soalan " +noSoalanCounter+" :</label><input type=text placeholder='Soalan " +noSoalanCounter+"' id='soalan" +noSoalanCounter+"' name='soalan" +noSoalanCounter+"'><br><br><div class=jawapanBox><label for=jawapan> Jawapan :</label><select name='jawapan" +noSoalanCounter+"' id='jawapan" +noSoalanCounter+"'><option value=A> A </option><option value=B> B </option><option value=C> C </option><option value=D> D </option></select> </div><br><br><br><br><br><div class=pilihanBox><div><label for=pilihanA style=float: left;> pilihan A :</label></div><div><input type=text placeholder='Pilihan A Soalan " +noSoalanCounter+"' id='pilihanA" +noSoalanCounter+"' name='pilihanA" +noSoalanCounter+"'></div><div><label for=pilihanB style=float: left;> pilihan B :</label></div><div><input type=text placeholder='Pilihan B Soalan " +noSoalanCounter+"' id='pilihanB" +noSoalanCounter+"' name='pilihanB" +noSoalanCounter+"'></div><div><label for=pilihanC style=float: left;> pilihan C :</label></div><div><input type=text placeholder='Pilihan C Soalan " +noSoalanCounter+"' id='pilihanC" +noSoalanCounter+"' name='pilihanC" +noSoalanCounter+"'></div><div><label for=pilihanD style=float: left;> pilihan D :</label></div><div><input type=text placeholder='Pilihan D Soalan " +noSoalanCounter+"' id='pilihanD" +noSoalanCounter+"' name='pilihanD" +noSoalanCounter+"'></div><br></div>";
+                        document.getElementById('soalanDitambah').appendChild(divForm);
 
                         
-                        /////////////////////////////////////////////////////////////////////////////////////////////
-                        // adding form
-
-                        //soalan form
-                        document.getElementById("soalanDitambah").appendChild(formSoalan);  
-                        document.getElementById("soalanDitambah").appendChild(br.cloneNode());
-
-                        // put dropdown ABCD for jawapan
-                        document.getElementById("soalanDitambah").appendChild(formJawapan);
-                        document.getElementById("soalanDitambah").appendChild(br.cloneNode());
-
-                        // pilihan form
-                        document.getElementById("soalanDitambah").appendChild(formPilihan);
-                        document.getElementById("soalanDitambah").appendChild(br.cloneNode());
+                        return false;
+                    }
 
 
+                    function deleteSoalan(){
+
+                        
+
+
+
+
+                        //last step is to subtract noSoalanCounter by 1
+                        if (noSoalanCounter > 1){
+                            noSoalanCounter--; 
+                        }
+
+                        return false;
+                        
                     }
                 </script>
 
