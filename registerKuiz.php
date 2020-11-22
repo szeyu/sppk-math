81b3848038a3293e36615588fbd7e7900e6495a3
szeyu <szeyusim@gmail.com>
2020-11-23 00:01:43 +0800

    still add some sql code

--- registerKuiz.php
+++ registerKuiz.php
@@ -13,7 +13,13 @@
                 $subTopik = $_POST['subTopik'];
                 $tajuk = $_POST['tajuk'];
                 $IdTopik = "T";       // note that the T later will concatenate with last number + 1
+                $intSoalan;
+                $intPilihan;
                 
+
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //                                   IdTopik
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 //to determine what is the last digit of latest IdTopik
                 $getLastDataTopik = "SELECT IdTopik FROM TOPIK ORDER BY IdTopik DESC LIMIT 1";
                 $resultTopik = mysqli_query($con ,$getLastDataTopik);
@@ -21,53 +27,79 @@
                 //echo($resultTopik);
 
                 if(mysqli_num_rows($resultTopik) == 0){
-                    $IdTopik = $IdTopik . "1";     // to make it become T1 which is our fisrt Id for Topik
+                    $IdTopik = $IdTopik . "1";     // to make it become T1 which is our fisrt Id for TOPIK
                 } else{
-                    if($row = mysqli_fetch_assoc($resultTopik)){
-                        //echo ($row['IdTopik']);
-                        $str = $row['IdTopik'];
-                        $str = ltrim ($str, 'T');     // to remove the T so that only number.
-                        //echo $str;
-                        $int = (int)$str;       // covert string to integer
-                        $int++;                 // to add new IdTopik that is unique
-                        //echo $int;
+                    if($rowTopik = mysqli_fetch_assoc($resultTopik)){
+                        //echo ($rowTopik['IdTopik']);
+                        $strTopik = $rowTopik['IdTopik'];
+                        $strTopik = ltrim ($strTopik, 'T');     // to remove the T so that only number.
+                        //echo $strTopik;
+                        $intTopik = (int)$strTopik;       // covert string to integer
+                        $intTopik++;                 // to add new IdTopik that is unique
+                        //echo $intTopik;
 
-                        $IdTopik = $IdTopik.$int; 
+                        $IdTopik = $IdTopik.$intTopik; 
                         //echo $IdTopik;
-                    }
-                        
+                    }             
                 }
                 //add data to TOPIK SQL
                 $addDataToTopik = "INSERT INTO TOPIK (IdTopik, tajuk, subTopik) VALUES ('".$IdTopik."','".$tajuk."','".$subTopik."')";
 
                 // add some query code here
-                //////////////////////////////////////////////////////////////////////
+                //////////// QUERY CODE /////////////////
 
 
-                //to determine what is the last digit of latest IdTopik
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //                                    IdSoalan
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //to determine what is the last digit of latest IdSoalan
                 $getLastDataSoalan = "SELECT IdSoalan FROM SOALAN ORDER BY IdSoalan DESC LIMIT 1";
                 $resultSoalan = mysqli_query($con ,$getLastDataSoalan);
-                if(mysqli_num_rows($resultTopik) == 0){
-                    $IdTopik = $IdTopik . "1";     // to make it become T1 which is our fisrt Id for Topik
+                if(mysqli_num_rows($resultSoalan) == 0){
+                    $intSoalan = 1;     // to make it become S1 which is our fisrt Id for SOALAN
                 } else{
-                    if($row = mysqli_fetch_assoc($resultTopik)){
-                        //echo ($row['IdTopik']);
-                        $str = $row['IdTopik'];
-                        $str = ltrim ($str, 'T');     // to remove the T so that only number.
-                        //echo $str;
-                        $int = (int)$str;       // covert string to integer
-                        $int++;                 // to add new IdTopik that is unique
-                        //echo $int;
+                    if($rowSoalan = mysqli_fetch_assoc($resultSoalan)){
+                        //echo ($rowSoalan['IdSoalan']);
+                        $strSoalan = $rowSoalan['IdSoalan'];
+                        $strSoalan = ltrim ($strSoalan, 'S');     // to remove the S so that only number.
+                        //echo $strSoalan;
+                        $intSoalan = (int)$strSoalan;       // covert string to integer
+                        //echo $intSoalan;
 
-                        $IdTopik = $IdTopik.$int; 
-                        //echo $IdTopik;
-                    }
-                        
+                    }             
                 }
 
-                ///////////////////////////////////////////////////////
-                //           note that after get IdSoalan then during loop just plus one to it because many soalan
-                //////////////////////////////////////////////////////
+
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //                                   IdPilihan
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //to determine what is the last digit of latest IdSPilihan
+                $getLastDataPilihan = "SELECT IdPilihan FROM PILIHAN ORDER BY IdPilihan DESC LIMIT 1";
+                $resultPilihan = mysqli_query($con ,$getLastDataPilihan);
+                if(mysqli_num_rows($resultPilihan) == 0){
+                    $intPilihan = 1;     // to make it become P1 which is our fisrt Id for PILIHAN
+                } else{
+                    if($rowPilihan = mysqli_fetch_assoc($resultPilihan)){
+                        //echo ($rowPilihan['IdPilihan']);
+                        $strPilihan = $rowPilihan['IdPilihan'];
+                        $strPilihan = ltrim ($strPilhan, 'P');     // to remove the P so that only number.
+                        //echo $strPilihan;
+                        $intPilihan = (int)$strPilihan;       // covert string to integer
+                        //echo $intPilihan;
+
+                    }             
+                }
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+                ///////////////////////////////////////////////////////////////////////////////////////////
+                //      note that after get IdSoalan then during loop just plus one to it because many soalan
+                //      Id Pilihan too!!
+                ///////////////////////////////////////////////////////////////////////////////////////////
 
                 $noSoalan = (count($_POST) - 3)/6;
                 for ($i=1; $i<=$noSoalan; $i++) {
@@ -86,16 +118,34 @@
 
 
                     //add some code to get the IdSoalan
-
-
+                    $intSoalan++;                 // to add new IdSoalan that is unique
+                    $IdSoalan = "S".$intSoalan;       // note that the S will concatenate with last number + 1
 
 
                     // add SQL here because this is loop
-                    $addDataToSoalan = "INSERT INTO SOALAN (IdSoalan, NoSoalan, soalan, IdTopik) VALUES (NULL,'".$i."','".$soalan."','".$IdTopik."')";
+                    // add data to SOALAN SQL
+                    $addDataToSoalan = "INSERT INTO SOALAN (IdSoalan, NoSoalan, soalan, IdTopik) VALUES ('".$IdSoalan."','".$i."','".$soalan."','".$IdTopik."')";
+                    
+                    //add query code here
+                    /////////// QUERY CODE //////////////
+
+
+                    ///////////////////////////////////////////////////
+                    //          at the same time, need add data to PILIHAN
+                    ///////////////////////////////////////////////////
+
+                    //add some code to get the IdPilihan
+                    $intSoalan++;                 // to add new IdSoalan that is unique
+                    $IdSoalan = "S".$intSoalan;       // note that the S will concatenate with last number + 1
+
                 } 
                 
-                // echo ('<script> alert("Soalan berjaya dimuat naik!"); </script>');
-                // header('Location: ./indexGuru.php');       // direct user to login page to login
+
+
+
+
+                echo ('<script> alert("Soalan berjaya dimuat naik!"); </script>');
+                header('Location: ./indexGuru.php');       // direct user to login page to login
             }
             
             //mysqli_close($con);        //disconnect
