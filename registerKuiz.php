83ad7c46727fff70015f5a888844930d0db3f4ee
szeyu <szeyusim@gmail.com>
2020-11-22 20:59:02 +0800

    done for the push topik

--- registerKuiz.php
+++ registerKuiz.php
@@ -15,19 +15,59 @@
                 $IdTopik = "T";       // note that the T later will concatenate with last number + 1
                 
                 //to determine what is the last digit of latest IdTopik
-                $getLastDataTopik = "SELECT IdTopik FROM TOPIK ORDER BY DESC LIMIT 1";
+                $getLastDataTopik = "SELECT IdTopik FROM TOPIK ORDER BY IdTopik DESC LIMIT 1";
                 $resultTopik = mysqli_query($con ,$getLastDataTopik);
+                //echo "helo";
+                //echo($resultTopik);
 
                 if(mysqli_num_rows($resultTopik) == 0){
                     $IdTopik = $IdTopik . "1";     // to make it become T1 which is our fisrt Id for Topik
                 } else{
-                    echo ($resultTopik);
-                }
+                    if($row = mysqli_fetch_assoc($resultTopik)){
+                        //echo ($row['IdTopik']);
+                        $str = $row['IdTopik'];
+                        $str = ltrim ($str, 'T');     // to remove the T so that only number.
+                        //echo $str;
+                        $int = (int)$str;       // covert string to integer
+                        $int++;                 // to add new IdTopik that is unique
+                        //echo $int;
 
+                        $IdTopik = $IdTopik.$int; 
+                        //echo $IdTopik;
+                    }
+                        
+                }
                 //add data to TOPIK SQL
-                $addDataToTopik = "INSERT INTO TOPIK (IdTopik, tajuk, subTopik) VALUES (NULL,'".$tajuk."','".$subTopik."')";
+                $addDataToTopik = "INSERT INTO TOPIK (IdTopik, tajuk, subTopik) VALUES ('".$IdTopik."','".$tajuk."','".$subTopik."')";
 
                 
+
+
+                //to determine what is the last digit of latest IdTopik
+                $getLastDataSoalan = "SELECT IdSoalan FROM SOALAN ORDER BY IdSoalan DESC LIMIT 1";
+                $resultSoalan = mysqli_query($con ,$getLastDataSoalan);
+                if(mysqli_num_rows($resultTopik) == 0){
+                    $IdTopik = $IdTopik . "1";     // to make it become T1 which is our fisrt Id for Topik
+                } else{
+                    if($row = mysqli_fetch_assoc($resultTopik)){
+                        //echo ($row['IdTopik']);
+                        $str = $row['IdTopik'];
+                        $str = ltrim ($str, 'T');     // to remove the T so that only number.
+                        //echo $str;
+                        $int = (int)$str;       // covert string to integer
+                        $int++;                 // to add new IdTopik that is unique
+                        //echo $int;
+
+                        $IdTopik = $IdTopik.$int; 
+                        //echo $IdTopik;
+                    }
+                        
+                }
+
+                ///////////////////////////////////////////////////////
+                //           note that after get IdSoalan then during loop just plus one to it because many soalan
+                //////////////////////////////////////////////////////
+
                 $noSoalan = (count($_POST) - 3)/6;
                 for ($i=1; $i<=$noSoalan; $i++) {
 
@@ -44,6 +84,11 @@
                     $pilihanD = $_POST["pilihanD$i"];
 
 
+                    //add some code to get the IdSoalan
+
+
+
+
                     // add SQL here because this is loop
                     $addDataToSoalan = "INSERT INTO SOALAN (IdSoalan, NoSoalan, soalan, IdTopik) VALUES (NULL,'".$i."','".$soalan."','".$IdTopik."')";
                 } 
@@ -52,9 +97,8 @@
                 // header('Location: ./indexGuru.php');       // direct user to login page to login
             }
             
-            mysqli_close($con);        //disconnect
+            //mysqli_close($con);        //disconnect
 
-            exit();
 
         ?>
     </body>
