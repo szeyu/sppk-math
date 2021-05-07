8b0445f81b8fc56202d0d497d10231ddeb9ff38b
szeyusim <szeyusim@gmail.com>
2021-05-07 13:40:17 +0800

    destroying indexguru

--- indexGuru.php
+++ indexGuru.php
@@ -18,38 +18,6 @@
         <link rel="icon" type="image/png" sizes="32x32" href="Image/favicon.ico">
         
 
-
-        <!--Banner-->
-        <div class="banner">
-            <div style="background-color:  #dadede;">
-                <p style="opacity:0;">.</p>
-                <h1 style="background-color:  #dadede;"> Ilmu Di Hujung Jari </h1>
-                <h2 style="background-color:  #dadede;"> Matematik Tingkatan 4 (DLP) </h2>
-                
-                <p style="color: #dadede;">.</p>
-            </div>
-            
-        </div>
-
-        <!--Top navigation bar-->
-        <div id="topnav" class="topnav">
-            <a class="active-button" onclick="homeGuru()">Laman Utama</a>
-            <a class="active-button" onclick="createGuru()">Sedia Kuiz</a>     
-            <a class="active-button" onclick="collectionGuru()">Koleksi Kuiz</a>
-            <a class="active-button" onclick="checkGuru()">Pantau Prestasi Murid</a>
-            <!--log out button-->
-            <div class="logOut">
-                <a href="login.php" onclick="return confirm('Log Keluar?')"> Log Keluar </a>   
-            </div> 
-        </div>
-
-
-        <!--add some js library-->
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-        crossorigin="anonymous" ></script>
-
-
         
         <script>
             var header = document.getElementById("topnav");
